4e526607ff1888cd8f6fdbad3f0294b030c581b7
mrpaulwoods <mr.paul.woods@gmail.com>
2025-08-09 19:04:19 -0500

    feat: added material-ui and react-router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import {BrowserRouter, Route, Routes} from "react-router";
+import {HomeIndexPage} from "./home/HomeIndexPage.tsx";
+import {CssBaseline} from "@mui/material";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App/>
-    </StrictMode>,
+        <CssBaseline/>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<HomeIndexPage/>}/>
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>
 )

ad8db2a0d8a9554f7d8c23cae4489e246e14c76d
mrpaulwoods <mr.paul.woods@gmail.com>
2025-08-09 22:37:29 -0500

    feat: conditionally import `UUID` based on field types in `UUIDTechnology`

--- backend/src/main/java/org/mrpaulwoods/backend/technology/UUIDTechnology.java
+++ backend/src/main/java/org/mrpaulwoods/backend/technology/UUIDTechnology.java
@@ -3,12 +3,20 @@ package org.mrpaulwoods.backend.technology;
 import org.mrpaulwoods.backend.generate.dto.AppRequest;
 import org.springframework.stereotype.Component;
 
+import static org.apache.commons.lang3.StringUtils.endsWithIgnoreCase;
+import static org.apache.commons.lang3.StringUtils.equalsIgnoreCase;
+
 @Component
 public class UUIDTechnology implements Technology {
 
     @Override
     public void importCodeBlock(AppRequest appRequest, StringBuilder sb) {
-        sb.append("import java.util.UUID;\n");
+
+        if (appRequest.getFields().stream().anyMatch(f ->
+                equalsIgnoreCase(f.getType(), "UUID") || endsWithIgnoreCase(f.getType(), ".UUID"))) {
+            sb.append("import java.util.UUID;\n");
+        }
+
     }
 
 }

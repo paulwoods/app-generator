f892ea784df52a939a36e1e8f0482f14db0e0bc2
mrpaulwoods <mr.paul.woods@gmail.com>
2025-08-15 21:26:16 -0500

    chore: cleanup

--- backend/src/main/java/org/mrpaulwoods/backend/utils/Constants.java
+++ backend/src/main/java/org/mrpaulwoods/backend/utils/Constants.java
@@ -1,8 +1,6 @@
 package org.mrpaulwoods.backend.utils;
 
 public class Constants {
-    public static final String SOURCE_FOLDER = "main";
-    public static final String TEST_FOLDER = "test";
     public static final String ENTITY_SUFFIX = ""; // Entity
     public static final String ENTITY_OBJECT = "entity";
     public static final String DTO_SUFFIX = "Dto";

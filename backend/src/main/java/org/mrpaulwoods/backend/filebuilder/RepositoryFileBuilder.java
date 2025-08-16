20f8b453cb1464ba240672e194e61e331d212db0
mrpaulwoods <mr.paul.woods@gmail.com>
2025-08-15 20:01:13 -0500

    refactor: remove commented-out `UserRepository` code in `RepositoryFileBuilder`

--- backend/src/main/java/org/mrpaulwoods/backend/filebuilder/RepositoryFileBuilder.java
+++ backend/src/main/java/org/mrpaulwoods/backend/filebuilder/RepositoryFileBuilder.java
@@ -46,15 +46,3 @@ public final class RepositoryFileBuilder implements FileBuilder {
     }
 
 }
-/*
-package org.mrpaulwoods.sample1.repository;
-
-import org.mrpaulwoods.sample1.entity.User;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-import java.util.UUID;
-
-public interface UserRepository extends ReactiveCrudRepository<User, UUID> {
-}
-
- */

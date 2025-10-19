c7263fb5afd843e222f2c726a88c64e4b63fba08
mrpaulwoods <mr.paul.woods@gmail.com>
2025-10-18 19:32:25 -0500

    feat: add Prometheus monitoring to backend and configure Docker setup
    - Introduced Prometheus metrics export in GenerateController and updated application configuration.
    - Added prometheus.yml for monitoring configuration.
    - Created Dockerfile and docker-compose.yml for containerized deployment.

--- backend/src/main/java/org/mrpaulwoods/backend/generate/controller/GenerateController.java
+++ backend/src/main/java/org/mrpaulwoods/backend/generate/controller/GenerateController.java
@@ -1,7 +1,9 @@
 package org.mrpaulwoods.backend.generate.controller;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.mrpaulwoods.backend.generate.dto.AppRequest;
 import org.mrpaulwoods.backend.generate.dto.GenerateResults;
@@ -12,18 +14,45 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 @RestController
 @RequestMapping("/v1/generate")
 @Slf4j
-@RequiredArgsConstructor
 public class GenerateController {
 
     private final GenerateService generateService;
+    private final MeterRegistry registry;
+    private final Counter processedCounter;
+    private final Timer processingTimer;
+
+    public GenerateController(
+            GenerateService generateService,
+            MeterRegistry registry
+    ) {
+        this.generateService = generateService;
+        this.registry = registry;
+
+        this.processedCounter = Counter
+                .builder("generate.controller.generate.count")
+                .description("Total greetings processed")
+                .tag("component", "service")
+                .register(registry);
+
+        this.processingTimer = Timer
+                .builder("generate.controller.generate.timer")
+                .description("Greeting processing duration")
+                .tag("component", "service")
+                .publishPercentileHistogram(true)     // enables histogram buckets for Prometheus
+                .maximumExpectedValue(Duration.ofSeconds(5))
+                .register(registry);
+    }
 
     @PostMapping
     public Mono<GenerateResults> generate(@Valid @RequestBody AppRequest appRequest) {
         log.info("generate: {}", appRequest);
-        return generateService.generate(appRequest);
+        processedCounter.increment();
+        return processingTimer.record(() -> generateService.generate(appRequest));
     }
     
 }
